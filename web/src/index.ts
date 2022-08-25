b25b6569e699adb11f9e0d430cf098233a9e184f
Daniel Choudhury <dannychoudhury@gmail.com>
2022-08-25 18:17:00 +0100

    Update testing code

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,22 @@
 const Bazinga = {
-	name: window?.location.hostname ?? 'Bazinga'
+  name: window?.location.hostname ?? 'Bazinga',
+  urlSearchParams: new URLSearchParams({ foo: 'bar' }), // not supported by ie11
+  mySymbol: Symbol('mySymbol'),
+  repeatedString: 'repeatedString'.repeat(10),
+  myMap: new Map(),
+};
+
+Promise.any([Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]).then(
+  console.log
+);
+Promise.any([Promise.reject(1), Promise.reject(2), Promise.reject(3)]).catch(
+  ({ errors }) => console.log(errors)
+);
+
+for (let [_, d, D] of '1111a2b3cccc'.matchAll(/(\d)(\D)/g)) {
+  console.log(d, D);
 }
+
+
 
 export default Bazinga
